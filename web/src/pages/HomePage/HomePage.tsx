0e4124d6f10489c8788651962da79d4a121e3882
ladderschool <ladderschooledu@gmail.com>
2024-02-22 14:24:40 -0500

    0.0.1_init

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,14 @@
-import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
 const HomePage = () => {
   return (
     <>
-      <Metadata title="Home" description="Home page" />
+      <Metadata
+        title="WoW <Nakama> Guild Page"
+        description="Website for Nakama WoW guild"
+      />
 
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <div>HomePage</div>
     </>
   )
 }
